353ddf55e8da27c4d86d7d9ad657e2dd2c020463
sandhya12r <126336441+sandhya12r@users.noreply.github.com>
2023-05-31 13:23:34 +0530

    portfolio

--- script.js
+++ script.js
@@ -18,12 +18,27 @@ window.onscroll =() =>{
     }
 }
 
-function loader(){
-    document.querySelector('.loader-container').classList.add('fade-out');
-}
+function sendEmail(){
+    Email.send({
+      SecureToken: "e7787bc4-d762-4c7b-a4f1-6e1128da1112",
+      To : 'prajapatis1201@gmail.com',
+      From : "prajapatis1201@gmail.com",
+      Subject : document.getElementById('subject').value,
+      Body : "Name: "+ document.getElementById('name').value
+            +"<br> Email:" + document.getElementById('email').value
+            +"<br> Message:" + document.getElementById('message').value
+    }).then(
+      message => alert(message)
+    );
+  }
+//e7787bc4-d762-4c7b-a4f1-6e1128da1112
+//087366F428507D7C0090CAA3D6FD9C8D3305
+// function loader(){
+//     document.querySelector('.loader-container').classList.add('fade-out');
+// }
 
-function fadeOut(){
-    setInterval(loader , 1500);
-}
+// function fadeOut(){
+//     setInterval(loader , 1500);
+// }
 
-window.onload = fadeOut();
+// window.onload = fadeOut();
